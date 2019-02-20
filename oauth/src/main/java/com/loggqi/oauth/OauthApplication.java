4ad38966a421bfe4dc91c42c0004e70386a09219
howard1994 <565449577@qq.com>
2019-02-20 10:14:33 +0800

    XXX

--- oauth/src/main/java/com/loggqi/oauth/OauthApplication.java
+++ oauth/src/main/java/com/loggqi/oauth/OauthApplication.java
@@ -7,7 +7,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableEurekaClient
-public class OauthApplication {
+public class  OauthApplication {
     public static void main(String[] args) {
         SpringApplication.run(OauthApplication.class, args);
     }
